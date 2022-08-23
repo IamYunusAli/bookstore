c2e41079321918a114b0b6bdb808d85667c42ade
iamyunusali <aliyunus291@gmail.com>
2022-08-23 10:46:09 +0300

    Add checkstatus functionaluty

--- src/pages/Catagorypage.js
+++ src/pages/Catagorypage.js
@@ -1,9 +1,20 @@
 import './pages.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { CHECK } from '../redux/catagories/catagories';
 
-const Catagories = () => (
-  <div className="catagory">
-    <button className="buttons" type="button">check status</button>
-  </div>
-);
+const Catagories = () => {
+  const output = useSelector((state) => state.catagories);
+  const dispatch = useDispatch();
+
+  const catagButton = () => {
+    dispatch(CHECK());
+  };
+  return (
+    <div className="catagory">
+      <button className="buttons" type="button" onClick={catagButton}>check status</button>
+      <h4>{output}</h4>
+    </div>
+  );
+};
 
 export default Catagories;
