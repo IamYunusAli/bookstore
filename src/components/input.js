738a4bb27db5bd37611976631b096c6b2cf164b8
iamyunusali <aliyunus291@gmail.com>
2022-08-23 10:45:20 +0300

    Add book add functionality

--- src/components/input.js
+++ src/components/input.js
@@ -1,19 +1,53 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import { v4 as uuidv4 } from 'uuid';
+import { ADDBOOKS } from '../redux/books/book';
 import './components.css';
 
-const Input = () => (
-  <div className="inputform">
-    <div className="form-divider" />
-    <h2>ADD NEW BOOK</h2>
-    <form className="form">
-      <input className="input titleinput" placeholder="Book Title" />
-      <select className="input selectinput">
-        <option> Scifi</option>
-        <option> Romantic</option>
-        <option> Fiction</option>
-      </select>
-      <button className="buttons addbook" type="submit"> Add Book</button>
-    </form>
-  </div>
-);
+const Input = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const Addboooks = (element) => {
+    element.preventDefault();
+    const book = {
+      id: uuidv4(),
+      title,
+      author,
+      percent: 12,
+      chapter: 1,
+      genere: 'Science Fiction',
+    };
+    if (title.length !== 0 && author.length !== 0) {
+      dispatch(ADDBOOKS(book));
+      setTitle('');
+      setAuthor('');
+    }
+  };
+  return (
+    <div className="inputform">
+      <div className="form-divider" />
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={Addboooks} className="form">
+        <input
+          onChange={(element) => setTitle(element.target.value)}
+          value={title}
+          className="input titleinput"
+          placeholder="Book Title"
+          required
+        />
+        <input
+          onChange={(element) => setAuthor(element.target.value)}
+          value={author}
+          className="input titleinput"
+          placeholder="Book author"
+          required
+        />
+        <button type="submit" className="buttons addbook"> Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default Input;
