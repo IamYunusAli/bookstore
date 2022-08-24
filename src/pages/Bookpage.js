0b2936325df4b229e523cb6024b9e554827ab92a
iamyunusali <aliyunus291@gmail.com>
2022-08-24 10:52:11 +0300

    display fetched data

--- src/pages/Bookpage.js
+++ src/pages/Bookpage.js
@@ -1,21 +1,26 @@
-import { useSelector } from 'react-redux/es/exports';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
 import Input from '../components/input';
 import Books from '../components/Book';
+import { fetchBooks } from '../redux/books/book';
 import './pages.css';
 
 const Bookpage = () => {
   const bookList = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
   return (
     <div className="bookcont">
       {bookList.map((book) => (
         <Books
-          key={book.id}
-          id={book.id}
-          genere={book.genere}
-          title={book.title}
-          author={book.author}
-          percent={book.percent}
-          chapter={book.chapter}
+          key={book[0]}
+          id={book[0]}
+          category={book[1][0].category}
+          title={book[1][0].title}
+          author={book[1][0].author}
         />
       ))}
 
